7710136b7b01dbc4c67ba624403524dd4eb88853
oznchex <114240463+oznchex@users.noreply.github.com>
2025-03-20 21:11:11 +0900

    feat: first commit

---
+++ src/main/java/com/liaison/groble/GrobleApplication.java
@@ -0,0 +1,13 @@
+package com.liaison.groble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrobleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrobleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liaison/groble/GrobleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaison.groble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrobleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
