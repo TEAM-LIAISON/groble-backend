53e269f24f131b634e18e7df57b60d91bfc40417
oznchex <114240463+oznchex@users.noreply.github.com>
2025-03-26 14:13:02 +0900

    feat: 추가 OAuth2 Provider 구현
    - 네이버와 구글 provider에 대한 UserInfo 클래스 추가

---
+++ groble-auth/src/main/java/liaison/grobleauth/security/oauth2/provider/NaverOAuth2UserInfo.java
@@ -0,0 +1,53 @@
+package liaison.grobleauth.security.oauth2.provider;
+
+import java.util.Map;
+
+import liaison.grobleauth.security.oauth2.OAuth2UserInfo;
+
+public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+  @SuppressWarnings("unchecked")
+  public NaverOAuth2UserInfo(Map<String, Object> attributes) {
+    super(attributes);
+  }
+
+  @Override
+  public String getId() {
+    Map<String, Object> response = getResponse();
+    if (response == null) {
+      return null;
+    }
+    return (String) response.get("id");
+  }
+
+  @Override
+  public String getName() {
+    Map<String, Object> response = getResponse();
+    if (response == null) {
+      return null;
+    }
+    return (String) response.get("name");
+  }
+
+  @Override
+  public String getEmail() {
+    Map<String, Object> response = getResponse();
+    if (response == null) {
+      return null;
+    }
+    return (String) response.get("email");
+  }
+
+  @Override
+  public String getImageUrl() {
+    Map<String, Object> response = getResponse();
+    if (response == null) {
+      return null;
+    }
+    return (String) response.get("profile_image");
+  }
+
+  @SuppressWarnings("unchecked")
+  private Map<String, Object> getResponse() {
+    return (Map<String, Object>) attributes.get("response");
+  }
+}
