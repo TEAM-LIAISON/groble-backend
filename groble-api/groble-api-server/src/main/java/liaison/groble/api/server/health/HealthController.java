b31f246a3ca70ac3613b81271754529c7d672dfe
oznchex <114240463+oznchex@users.noreply.github.com>
2025-04-09 00:33:42 +0900

    fix: @Value 값 잘못 선언했던 오류 수정

--- groble-api/groble-api-server/src/main/java/liaison/groble/api/server/health/HealthController.java
+++ groble-api/groble-api-server/src/main/java/liaison/groble/api/server/health/HealthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class HealthController {
 
-  @Value("${server.env")
+  @Value("${server.env}")
   private String env;
 
   @GetMapping("/hc")
