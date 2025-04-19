d5bb66f6a1c46936692fb17a22a2e9bdd7beb500
oznchex <114240463+oznchex@users.noreply.github.com>
2025-04-19 22:42:28 +0900

    feat: Product 엔티티 추가

--- groble-domain/src/main/java/liaison/groble/domain/product/entity/Product.java
+++ groble-domain/src/main/java/liaison/groble/domain/product/entity/Product.java
@@ -1,3 +1,41 @@
 package liaison.groble.domain.product.entity;
 
-public class Product {}
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private String category;
+
+  private String price;
+
+  private String stock;
+
+  private String sellerId;
+
+  @ToString.Exclude private String imageUrl;
+}
