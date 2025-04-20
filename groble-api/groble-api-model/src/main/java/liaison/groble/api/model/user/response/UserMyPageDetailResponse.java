8bb7fe3f8abb7e6c050a48f882f4159a59178f2a
oznchex <114240463+oznchex@users.noreply.github.com>
2025-04-20 22:26:24 +0900

    refactor: 마이페이지 요약 및 상세 조회에서 응답 명세 작성
    - 각 케이스 및 예시 응답 명세서 작성

--- groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/UserMyPageDetailResponse.java
+++ groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/UserMyPageDetailResponse.java
@@ -2,6 +2,7 @@ package liaison.groble.api.model.user.response;
 
 import liaison.groble.common.response.EnumResponse;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,11 +13,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserMyPageDetailResponse {
+  @Schema(description = "사용자 닉네임", example = "권동민")
   private String nickName;
+
+  @Schema(
+      description = "사용자 계정 정보 객체 (INTEGRATED - 통합 계정, SOCIAL - 소셜 계정)",
+      example = "{ " + "\"code\": \"INTEGRATED\", " + "\"description\": \"통합 계정\" }")
   private EnumResponse accountType;
+
+  @Schema(
+      description = "소셜 계정에 대한 플랫폼 정보",
+      example = "{ " + "\"code\": \"KAKAO\", " + "\"description\": \"카카오\" }")
   private EnumResponse providerType;
+
+  @Schema(description = "이메일", example = "kwondm7@naver.com")
   private String email;
+
+  @Schema(description = "사용자 프로필 이미지 URL", example = "https://example.com/profile.jpg")
   private String profileImageUrl;
+
+  @Schema(description = "사용자 전화번호", example = "010-1234-5678")
   private String phoneNumber;
+
+  @Schema(description = "판매자 계정 미생성 여부", example = "true")
   private boolean sellerAccountNotCreated;
 }
--- groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/UserMyPageSummaryResponse.java
+++ groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/UserMyPageSummaryResponse.java
@@ -2,6 +2,7 @@ package liaison.groble.api.model.user.response;
 
 import liaison.groble.common.response.EnumResponse;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,9 +12,20 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "마이페이지 요약 정보 응답")
 public class UserMyPageSummaryResponse {
+
+  @Schema(description = "사용자 닉네임", example = "권동민")
   private String nickName;
+
+  @Schema(description = "사용자 프로필 이미지 URL", example = "https://example.com/profile.jpg")
   private String profileImageUrl;
+
+  @Schema(
+      description = "사용자 유형 정보 객체 (BUYER - 구매자, SELLER - 판매자)",
+      example = "{ " + "\"code\": \"BUYER\", " + "\"description\": \"구매자\" }")
   private EnumResponse userType;
+
+  @Schema(description = "판매자 계정 전환 가능 여부", example = "true")
   private boolean canSwitchToSeller;
 }
