88347ac2b9893f499cda49466575cd2358d90839
oznchex <114240463+oznchex@users.noreply.github.com>
2025-04-24 21:36:05 +0900

    refactor(domain): 컨텐츠 타입에 대한 데이터 리팩토링

--- groble-domain/src/main/java/liaison/groble/domain/gig/entity/DocumentOption.java
+++ groble-domain/src/main/java/liaison/groble/domain/gig/entity/DocumentOption.java
@@ -3,9 +3,6 @@ package liaison.groble.domain.gig.entity;
 import static jakarta.persistence.EnumType.STRING;
 import static lombok.AccessLevel.PROTECTED;
 
-import java.math.BigDecimal;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
@@ -20,16 +17,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 public class DocumentOption extends GigOption {
+  // 컨텐츠 제공 방식
   @Enumerated(value = STRING)
   private ContentDeliveryMethod contentDeliveryMethod;
-
-  // 옵션 이름
-  @Column(name = "option_name")
-  private String name;
-
-  // 옵션 설명
-  @Column(name = "option_description")
-  private String description;
-
-  private BigDecimal price;
 }
