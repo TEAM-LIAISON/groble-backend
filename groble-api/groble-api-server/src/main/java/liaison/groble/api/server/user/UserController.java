0a8519047e309c8f6a1dfcb1f6e143205cf0bd83
oznchex <114240463+oznchex@users.noreply.github.com>
2025-05-06 14:51:35 +0900

    docs: 마이페이지 조회 및 가입 유형 전환에 대한 명세 정의 수정

--- groble-api/groble-api-server/src/main/java/liaison/groble/api/server/user/UserController.java
+++ groble-api/groble-api-server/src/main/java/liaison/groble/api/server/user/UserController.java
@@ -31,7 +31,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 /** 사용자 정보 관련 API 컨트롤러 */
 @RestController
 @RequestMapping("/api/v1/users")
-@Tag(name = "사용자 정보 API", description = "닉네임, 비밀번호 설정 및 계정 전환 API")
+@Tag(name = "사용자 정보 API", description = "마이페이지 조회 및 가입 유형 전환 API")
 public class UserController {
 
   private final UserService userService;
