cee7b4812420bed13b33aabd21f8f6947b56f3c9
oznchex <114240463+oznchex@users.noreply.github.com>
2025-05-07 00:31:14 +0900

    fix: 마이페이지 요약 API Swagger 어노테이션 다형성 응답 구조 수정
    마이페이지 요약 API의 Swagger 문서화 문제를 해결했습니다.
    
    - oneOf 속성 사용으로 인해 응답 필드가 data 객체 내부가 아닌 루트 레벨에
      노출되는 문제 수정
    - Jackson의 @JsonTypeInfo와 @JsonSubTypes를 활용한 다형성 응답 처리 구현
    - UserMyPageSummaryResponse 인터페이스를 도입하여 구매자/판매자 응답 타입을
      통합적으로 관리
    - 예제 응답 형식을 유지하여 API 문서의 명확성 보장
    - GrobleResponse 응답 패턴 일관성 유지

--- groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/swagger/MyPageSummary.java
+++ groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/swagger/MyPageSummary.java
@@ -7,6 +7,7 @@ import java.lang.annotation.Target;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -23,7 +24,48 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
       content =
           @Content(
               mediaType = "application/json",
-              schema = @Schema(implementation = UserMyPageSummaryApiResponse.class))),
+              schema = @Schema(implementation = UserMyPageSummaryApiResponse.class),
+              examples = {
+                @ExampleObject(
+                    name = "구매자 응답",
+                    summary = "구매자 마이페이지 요약 정보",
+                    value =
+                        """
+                                                {
+                                                  "status": "SUCCESS",
+                                                  "code": 200,
+                                                  "message": "요청이 성공적으로 처리되었습니다.",
+                                                  "data": {
+                                                    "nickname": "권동민",
+                                                    "profileImageUrl": null,
+                                                    "userType": "BUYER",
+                                                    "canSwitchToSeller": false
+                                                  },
+                                                  "timestamp": "2025-05-06 04:26:26"
+                                                }
+                                                """),
+                @ExampleObject(
+                    name = "판매자 응답",
+                    summary = "판매자 마이페이지 요약 정보",
+                    value =
+                        """
+                                                {
+                                                  "status": "SUCCESS",
+                                                  "code": 200,
+                                                  "message": "요청이 성공적으로 처리되었습니다.",
+                                                  "data": {
+                                                    "nickname": "김판매",
+                                                    "profileImageUrl": "https://example.com/profile.jpg",
+                                                    "userType": "SELLER",
+                                                    "verificationStatus": {
+                                                      "code": "APPROVED",
+                                                      "description": "승인됨"
+                                                    }
+                                                  },
+                                                  "timestamp": "2025-05-06 04:26:26"
+                                                }
+                                                """)
+              })),
   @ApiResponse(responseCode = "401", description = "인증 실패 (AccessToken 만료 또는 없음)"),
   @ApiResponse(responseCode = "404", description = "사용자 정보를 찾을 수 없음")
 })
--- groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/swagger/UserMyPageSummaryApiResponse.java
+++ groble-api/groble-api-model/src/main/java/liaison/groble/api/model/user/response/swagger/UserMyPageSummaryApiResponse.java
@@ -1,12 +1,9 @@
 package liaison.groble.api.model.user.response.swagger;
 
-import liaison.groble.api.model.user.response.BuyerMyPageSummaryResponse;
-import liaison.groble.api.model.user.response.SellerMyPageSummaryResponse;
+import liaison.groble.api.model.user.response.UserMyPageSummaryResponse;
 import liaison.groble.common.response.GrobleResponse;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(
-    name = "UserMyPageSummaryApiResponse",
-    oneOf = {BuyerMyPageSummaryResponse.class, SellerMyPageSummaryResponse.class})
-public class UserMyPageSummaryApiResponse extends GrobleResponse<Object> {}
+@Schema(name = "UserMyPageSummaryApiResponse")
+public class UserMyPageSummaryApiResponse extends GrobleResponse<UserMyPageSummaryResponse> {}
