6212f9643c9572be1983f85d0becdeb531a7e3b5
oznchex <114240463+oznchex@users.noreply.github.com>
2025-05-13 14:26:23 +0900

    fix: 실행 오류 수정

--- groble-api/groble-api-server/src/main/java/liaison/groble/api/server/notification/NotificationController.java
+++ groble-api/groble-api-server/src/main/java/liaison/groble/api/server/notification/NotificationController.java
@@ -1,16 +1,10 @@
 package liaison.groble.api.server.notification;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import liaison.groble.api.model.notification.response.NotificationItems;
 import liaison.groble.api.server.notification.mapper.NotificationDtoMapper;
-import liaison.groble.application.notification.dto.NotificationItemsDto;
 import liaison.groble.application.notification.service.NotificationService;
-import liaison.groble.common.annotation.Auth;
-import liaison.groble.common.model.Accessor;
-import liaison.groble.common.response.GrobleResponse;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -33,12 +27,4 @@ public class NotificationController {
   // 알림 단일 삭제
 
   // 알림 전체 조회
-  public ResponseEntity<GrobleResponse<NotificationItems>> getNotificationItems(
-      @Auth final Accessor accessor) {
-    NotificationItemsDto notificationItemsDto =
-        notificationService.getNotificationItems(accessor.getUserId());
-    NotificationItems notificationItems =
-        notificationDtoMapper.toNotificationItems(notificationItemsDto);
-    return ResponseEntity.ok(GrobleResponse.success(response, "알림 목록 전체 조회 성공"));
-  }
 }
