8eeaa8f1fb737792585a6ce89246448f5e8dfd45
oznchex <114240463+oznchex@users.noreply.github.com>
2025-06-03 01:52:52 +0900

    docs: 개인 및 법인 사업자의 메이커 계좌 인증 / 개인 메이커 계좌 인증 API 명세 추가

--- groble-api/groble-api-model/src/main/java/liaison/groble/api/model/auth/request/VerifyPersonalMakerAccountRequest.java
+++ groble-api/groble-api-model/src/main/java/liaison/groble/api/model/auth/request/VerifyPersonalMakerAccountRequest.java
@@ -15,14 +15,20 @@ import lombok.Setter;
 @Schema(description = "개인 메이커 인증 처리 요청")
 public class VerifyPersonalMakerAccountRequest {
   /** 정산받을 은행 계좌 소유자 이름 */
+  @Schema(description = "정산받을 은행 계좌 소유자 이름", example = "홍길동")
   private String bankAccountOwner;
 
   /** 정산받을 은행명 */
+  @Schema(description = "정산받을 은행명", example = "신한은행")
   private String bankName;
 
   /** 정산받을 계좌번호 */
+  @Schema(description = "정산받을 계좌번호", example = "110123456789")
   private String bankAccountNumber;
 
   /** 통장 사본 첨부 URL */
+  @Schema(
+      description = "통장 사본 첨부 URL",
+      example = "https://image.dev.groble.im/bankbook/honggildong.png")
   private String copyOfBankbookUrl;
 }
