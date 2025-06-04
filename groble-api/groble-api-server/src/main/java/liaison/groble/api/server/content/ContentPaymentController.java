30f14afa85216cf79fd052ae225e4ff645ce5cac
oznchex <114240463+oznchex@users.noreply.github.com>
2025-06-04 17:38:01 +0900

    docs: 결제 페이지 조회 로직에 명세서 예시 추가

--- groble-api/groble-api-server/src/main/java/liaison/groble/api/server/content/ContentPaymentController.java
+++ groble-api/groble-api-server/src/main/java/liaison/groble/api/server/content/ContentPaymentController.java
@@ -16,6 +16,9 @@ import liaison.groble.common.model.Accessor;
 import liaison.groble.common.response.GrobleResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,8 +34,42 @@ public class ContentPaymentController {
   private final CouponService couponService;
 
   @Operation(
-      summary = "콘텐츠 결제 페이지 정보 조회",
-      description = "콘텐츠 결제 페이지 정보를 조회합니다. 인증된 사용자만 쿠폰 정보를 포함하여 응답합니다.")
+      summary = "콘텐츠 결제 페이지 조회",
+      description = "콘텐츠 결제 페이지 렌더링에 필요한 정보를 조회합니다",
+      responses = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "성공",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    examples =
+                        @ExampleObject(
+                            value =
+                                """
+                    {
+                      "success": true,
+                      "data": {
+                        "thumbnailUrl": "https://example.com/thumbnail.jpg",
+                        "sellerName": "홍길동",
+                        "title": "프로그래밍 입문 과정",
+                        "contentType": "COACHING",
+                        "optionName": "1:1 개인 코칭 (1개월)",
+                        "price": 150000,
+                        "userCoupons": [
+                          {
+                            "couponCode": "WELCOME10",
+                            "name": "신규 가입 쿠폰",
+                            "couponType": "PERCENTAGE",
+                            "discountValue": 10,
+                            "validUntil": "2024-12-31T23:59:59",
+                            "minOrderPrice": 50000
+                          }
+                        ]
+                      }
+                    }
+                    """)))
+      })
   @GetMapping("/content/{contentId}/pay/{optionId}")
   public ResponseEntity<GrobleResponse<ContentPayPageResponse>> getContentPayPage(
       @Auth(required = false) Accessor accessor,
