f16bd6dacc08fe4a3a20a43d75b79e0ff58589e2
oznchex <114240463+oznchex@users.noreply.github.com>
2025-06-05 14:48:42 +0900

    refactor: Purchase 엔티티 생성 및 관리 추가, 트랜잭션 관리 개선
    - 결제 취소 기능 구현 (payRefund 사용)

---
+++ groble-infrastructure/groble-persistence/src/main/java/liaison/groble/persistence/purchase/PurchaseRepositoryImpl.java
@@ -0,0 +1,27 @@
+package liaison.groble.persistence.purchase;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import liaison.groble.domain.order.entity.Order;
+import liaison.groble.domain.purchase.entity.Purchase;
+import liaison.groble.domain.purchase.repository.PurchaseRepository;
+
+import lombok.AllArgsConstructor;
+
+@Repository
+@AllArgsConstructor
+public class PurchaseRepositoryImpl implements PurchaseRepository {
+  private final JpaPurchaseRepository jpaPurchaseRepository;
+
+  @Override
+  public Purchase save(Purchase purchase) {
+    return jpaPurchaseRepository.save(purchase);
+  }
+
+  @Override
+  public Optional<Purchase> findByOrder(Order order) {
+    return jpaPurchaseRepository.findByOrder(order);
+  }
+}
