0f741a034edb12bfc6b3b96cf80013c9f1485500
oznchex <114240463+oznchex@users.noreply.github.com>
2025-06-12 17:07:23 +0900

    feat: 관리자 페이지 주문 목록 전체 조회 기능 추가
    - todo: 상태 관리 enum 데이터 추가 필요
    - 코칭 진행 전, 코칭 진행 완료, 자료 전달 완료 등의 상태는 관리자 페이지에 추가 필요

---
+++ groble-application/src/main/java/liaison/groble/application/admin/dto/AdminOrderSummaryInfoDto.java
@@ -0,0 +1,18 @@
+package liaison.groble.application.admin.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AdminOrderSummaryInfoDto {
+  private LocalDateTime createdAt;
+  private String contentType;
+  private String purchaserName;
+  private String contentTitle;
+  private BigDecimal finalPrice;
+  private String orderStatus;
+}
--- groble-application/src/main/java/liaison/groble/application/admin/service/AdminOrderService.java
+++ groble-application/src/main/java/liaison/groble/application/admin/service/AdminOrderService.java
@@ -1,9 +1,16 @@
 package liaison.groble.application.admin.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import liaison.groble.application.admin.dto.AdminOrderCancellationReasonDto;
+import liaison.groble.application.admin.dto.AdminOrderSummaryInfoDto;
 import liaison.groble.application.order.service.OrderReader;
+import liaison.groble.common.response.PageResponse;
+import liaison.groble.domain.order.dto.FlatAdminOrderSummaryInfoDTO;
 import liaison.groble.domain.order.entity.Order;
 
 import lombok.RequiredArgsConstructor;
@@ -15,6 +22,23 @@ import lombok.extern.slf4j.Slf4j;
 public class AdminOrderService {
   private final OrderReader orderReader;
 
+  // 모든 주문 목록 전체 조회 메서드
+  public PageResponse<AdminOrderSummaryInfoDto> getAllOrders(Pageable pageable) {
+    Page<FlatAdminOrderSummaryInfoDTO> orderPage = orderReader.getAllOrders(pageable);
+
+    List<AdminOrderSummaryInfoDto> items =
+        orderPage.getContent().stream().map(this::convertFlatDtoToInfoResponse).toList();
+
+    PageResponse.MetaData meta =
+        PageResponse.MetaData.builder()
+            .sortBy(pageable.getSort().iterator().next().getProperty())
+            .sortDirection(pageable.getSort().iterator().next().getDirection().name())
+            .build();
+
+    return PageResponse.from(orderPage, items, meta);
+  }
+
+  // 주문 최소 사유 조회 메서드
   public AdminOrderCancellationReasonDto getOrderCancellationReason(String merchantUid) {
 
     Order order = orderReader.getOrderByMerchantUid(merchantUid);
@@ -23,5 +47,16 @@ public class AdminOrderService {
     }
 
     return AdminOrderCancellationReasonDto.builder().cancelReason(order.getOrderNote()).build();
+  }
+
+  private AdminOrderSummaryInfoDto convertFlatDtoToInfoResponse(FlatAdminOrderSummaryInfoDTO flat) {
+    return AdminOrderSummaryInfoDto.builder()
+        .createdAt(flat.getCreatedAt())
+        .contentType(flat.getContentType())
+        .purchaserName(flat.getPurchaserName())
+        .contentTitle(flat.getContentTitle())
+        .finalPrice(flat.getFinalPrice())
+        .orderStatus(flat.getOrderStatus())
+        .build();
   }
 }
