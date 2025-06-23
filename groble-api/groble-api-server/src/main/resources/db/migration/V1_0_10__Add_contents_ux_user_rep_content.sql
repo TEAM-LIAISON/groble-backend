820cb40c0901fde428998ec41e38f4674f12474f
oznchex <114240463+oznchex@users.noreply.github.com>
2025-06-24 02:17:49 +0900

    fix: ux 인덱스를 임시 제거

--- groble-api/groble-api-server/src/main/resources/db/migration/V1_0_10__Add_contents_ux_user_rep_content.sql
+++ groble-api/groble-api-server/src/main/resources/db/migration/V1_0_10__Add_contents_ux_user_rep_content.sql
@@ -1,7 +1,3 @@
 -- MySQL용 수정된 스크립트
 ALTER TABLE contents
 ADD COLUMN is_representative BOOLEAN NOT NULL DEFAULT FALSE;
-
--- MySQL 8.0+ 함수 기반 유니크 인덱스
-CREATE UNIQUE INDEX ux_user_representative_content
-ON contents(user_id, (CASE WHEN is_representative = TRUE THEN 1 ELSE NULL END));
