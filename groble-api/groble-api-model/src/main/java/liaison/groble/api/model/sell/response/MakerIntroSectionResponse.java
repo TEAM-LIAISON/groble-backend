b7e63eb4d8c50254e0cd72f5631f9370666058ae
oznchex <114240463+oznchex@users.noreply.github.com>
2025-06-23 14:01:55 +0900

    fix: seller_profiles 테이블 제거
    - 불필요한 테이블 제거

--- groble-api/groble-api-model/src/main/java/liaison/groble/api/model/sell/response/MakerIntroSectionResponse.java
+++ groble-api/groble-api-model/src/main/java/liaison/groble/api/model/sell/response/MakerIntroSectionResponse.java
@@ -1,3 +1,18 @@
 package liaison.groble.api.model.sell.response;
 
-public class MakerIntroSectionResponse {}
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MakerIntroSectionResponse {
+  private String profileImageUrl;
+  private String marketName;
+}
---
+++ groble-api/groble-api-server/src/main/resources/db/migration/V1_0_6__Delete_seller_profiles.sql
@@ -0,0 +1,1 @@
+DROP table seller_profiles;
