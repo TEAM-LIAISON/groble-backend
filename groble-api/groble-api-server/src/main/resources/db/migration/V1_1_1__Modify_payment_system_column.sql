4a1f18406d455b10990a5f0a2fa6c359e38e2531
oznchex <114240463+oznchex@users.noreply.github.com>
2025-07-09 16:15:00 +0900

    fix: 구매 테이블 기존 데이터 존재에 따른 flyway 쿼리 수정

--- groble-api/groble-api-server/src/main/resources/db/migration/V1_1_1__Modify_payment_system_column.sql
+++ groble-api/groble-api-server/src/main/resources/db/migration/V1_1_1__Modify_payment_system_column.sql
@@ -1,8 +1,13 @@
 ALTER TABLE orders
     DROP COLUMN cancel_reason;
 
+UPDATE purchases
+SET cancel_reason = 'ETC'
+WHERE cancel_reason IS NOT NULL
+  AND cancel_reason NOT IN ('OTHER_PAYMENT_METHOD', 'CHANGED_MIND', 'FOUND_CHEAPER_CONTENT', 'ETC');
+
 ALTER TABLE purchases
-    MODIFY COLUMN cancel_reason ENUM('OTHER_PAYMENT_METHOD', 'CHANGED_MIND', 'FOUND_CHEAPER_CONTENT', 'ETC') NULL;
+  MODIFY COLUMN cancel_reason ENUM('OTHER_PAYMENT_METHOD', 'CHANGED_MIND', 'FOUND_CHEAPER_CONTENT', 'ETC') NULL;
 
 ALTER TABLE purchases
     CHANGE COLUMN refund_requested_at cancel_requested_at datetime(6) NULL;
