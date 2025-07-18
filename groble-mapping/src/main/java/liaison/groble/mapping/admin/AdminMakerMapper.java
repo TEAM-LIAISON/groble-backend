c7b977090cd8468404550ae13f72e1b4fadad079
oznchex <114240463+oznchex@users.noreply.github.com>
2025-07-18 23:33:12 +0900

    fix: AdminMakerMapper config 설정 누락 오류 수정

--- groble-mapping/src/main/java/liaison/groble/mapping/admin/AdminMakerMapper.java
+++ groble-mapping/src/main/java/liaison/groble/mapping/admin/AdminMakerMapper.java
@@ -1,8 +1,12 @@
 package liaison.groble.mapping.admin;
 
+import org.mapstruct.Mapper;
+
 import liaison.groble.api.model.admin.response.AdminMakerDetailInfoResponse;
 import liaison.groble.application.admin.dto.AdminMakerDetailInfoDTO;
+import liaison.groble.mapping.config.GrobleMapperConfig;
 
+@Mapper(config = GrobleMapperConfig.class)
 public interface AdminMakerMapper {
   AdminMakerDetailInfoResponse toAdminMakerDetailInfoResponse(
       AdminMakerDetailInfoDTO adminMakerDetailInfoDTO);
