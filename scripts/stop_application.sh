b42535dfd18f8a5eca9e3f520863fd5c7e5536de
Yoon-Jemin <yjm1800@naver.com>
2025-07-19 11:13:21 +0900

    refactor: appspec.yml 파일 ECS 설정으로 수정

--- scripts/stop_application.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Stopping application..."
-
-# ECS 서비스 스케일다운 (옵션)
-# aws ecs update-service --cluster groble-cluster --service groble-prod-service --desired-count 0
-
-echo "Application stopped successfully"
