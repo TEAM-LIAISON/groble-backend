8c38bd5d924bc401f7b6247620c234594c324f6e
oznchex <114240463+oznchex@users.noreply.github.com>
2025-08-07 02:38:38 +0900

    config: externalApiRestTemplate 별도 추가

---
+++ groble-infrastructure/groble-external/src/main/java/liaison/groble/external/config/RestTemplateConfig.java
@@ -0,0 +1,29 @@
+package liaison.groble.external.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class RestTemplateConfig {
+
+  @Bean
+  @Primary
+  public RestTemplate restTemplate() {
+    log.info("기본 RestTemplate 생성");
+    SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+    factory.setConnectTimeout(30000); // 30초
+    factory.setReadTimeout(60000); // 60초
+    return new RestTemplate(factory);
+  }
+
+  @Bean("externalApiRestTemplate")
+  public RestTemplate externalApiRestTemplate() {
+    return restTemplate();
+  }
+}
