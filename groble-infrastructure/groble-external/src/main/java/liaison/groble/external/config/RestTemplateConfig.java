492d956cfb2b0c6f3319afc51b78c4816a0186c5
Yoon-Jemin <yjm1800@naver.com>
2025-08-07 12:13:07 +0900

    fix: restTemplate Bean 충돌 문제 해결

--- groble-infrastructure/groble-external/src/main/java/liaison/groble/external/config/RestTemplateConfig.java
+++ groble-infrastructure/groble-external/src/main/java/liaison/groble/external/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package liaison.groble.external.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
+@ConditionalOnProperty(name = "http.proxy.enabled", havingValue = "false", matchIfMissing = true)
 public class RestTemplateConfig {
 
   @Bean
