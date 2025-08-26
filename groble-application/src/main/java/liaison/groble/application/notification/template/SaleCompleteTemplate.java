30c2eb7cdcaf6c7a1584c7fc7fe1f0fe5a256814
oznchex <114240463+oznchex@users.noreply.github.com>
2025-08-27 01:51:59 +0900

    feat: 판매 알림 템플릿 추가

--- groble-application/src/main/java/liaison/groble/application/notification/template/SaleCompleteTemplate.java
+++ groble-application/src/main/java/liaison/groble/application/notification/template/SaleCompleteTemplate.java
@@ -1,10 +1,47 @@
 package liaison.groble.application.notification.template;
 
+import java.util.Collections;
+import java.util.List;
+import liaison.groble.application.notification.dto.KakaoNotificationDTO;
+import liaison.groble.application.notification.enums.KakaoNotificationType;
+import liaison.groble.external.infotalk.dto.message.ButtonInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SaleCompleteTemplate {
+public class SaleCompleteTemplate implements KakaoTemplate {
     @Value("${bizppurio.templates.sale-complete.code}")
-    private String saleCompleteTemplateCode;
+    private String templateCode;
+
+    @Override
+    public KakaoNotificationType getType() {
+        return KakaoNotificationType.SALE_COMPLETE; // 명확한 타입 지정
+    }
+
+    @Override
+    public String getCode() {
+        return templateCode;
+    }
+
+    @Override
+    public String getTitle() {
+        return "[Groble] 판매 알림";
+    }
+
+    @Override
+    public String buildMessage(KakaoNotificationDTO kakaoNotificationDTO) {
+        return MessageFormatter.saleComplete(kakaoNotificationDTO.getBuyerName(), kakaoNotificationDTO.getContentTitle(), kakaoNotificationDTO.getPrice());
+    }
+
+    @Override
+    public List<ButtonInfo> buildButtons(
+            KakaoNotificationDTO kakaoNotificationDTO, NotificationUrlBuilder urlBuilder) {
+        return Collections.singletonList(
+                ButtonInfo.builder()
+                        .name("확인하러 가기")
+                        .type("WL")
+                        .urlMobile(urlBuilder.getBaseUrl())
+                        .urlPc(urlBuilder.getBaseUrl())
+                        .build());
+    }
 }
