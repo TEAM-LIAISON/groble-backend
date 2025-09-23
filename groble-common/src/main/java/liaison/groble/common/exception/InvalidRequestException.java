ed65a93f79f82d09dd5715687012fceb5519840c
oznchex <114240463+oznchex@users.noreply.github.com>
2025-09-23 13:33:45 +0900

    hotfix: 비회원 결제 취소 승인/거절 오류 수정

--- groble-common/src/main/java/liaison/groble/common/exception/InvalidRequestException.java
+++ groble-common/src/main/java/liaison/groble/common/exception/InvalidRequestException.java
@@ -1,3 +1,14 @@
 package liaison.groble.common.exception;
 
-public class InvalidRequestException extends RuntimeException {}
+public class InvalidRequestException extends RuntimeException {
+
+  public InvalidRequestException() {}
+
+  public InvalidRequestException(String message) {
+    super(message);
+  }
+
+  public InvalidRequestException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
